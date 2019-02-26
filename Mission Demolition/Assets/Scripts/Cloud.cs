dca19a4081bc6da013c9a4f036ca448633291841
Isfand <izzywhizzzz@gmail.com>
2019-02-26 14:58:46 +0000

    cloud procedural genration created

---
+++ Mission Demolition/Assets/Scripts/Cloud.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Procedurally generating some clouds
+/// </summary>
+public class Cloud : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public GameObject cloudSphere;
+
+    public int numSpheresMin = 6; //Number of clouds to be create Min/ Max
+    public int numSpheresMax = 10;
+    public Vector3 sphereOffsetScale = new Vector3(5, 2, 1); // the maxmium distance that a cloudsphere could be from the centre of the cloud
+    public Vector3 sphereScaleRangeX = new Vector2(4, 8); //the ranges of scales, clouds are usually wider hence the larger x
+    public Vector2 sphereScaleRangeY = new Vector2(3, 4);
+    public Vector2 sphereScaleRangeZ = new Vector2(2, 4);
+    public float scaleYMin = 2f; //This prevents super skinnny clouds.
+
+    private List<GameObject> spheres;
+
+    private void Start()
+    {
+        spheres = new List<GameObject>(); //Holds a reference to all the sphers created for this cloud
+
+        int num = Random.Range(numSpheresMin, numSpheresMax); //number of spheres that should be attached to this one cloud
+        for (int i = 0; i < num; i++)
+        {
+            GameObject sphere = Instantiate<GameObject>(cloudSphere);
+            spheres.Add(sphere);
+            Transform sphereTrans = sphere.transform;
+            sphereTrans.SetParent(this.transform);//Set the spheres to be the child of this Cloud (the one in the hierachy) all set to [0,0,0]
+
+            //Randomly assign a postion
+            Vector3 offset = Random.insideUnitSphere; // a random point inside a sphere (within 1 unit of the [0,0,0] origin)
+            offset.x *= sphereOffsetScale.x;
+            offset.y *= sphereOffsetScale.y; //we then multiply it by our  offsets
+            offset.z *= sphereOffsetScale.z;
+            sphereTrans.localPosition = offset;// we use local position bc it takes in to account the centre of the parent as its centre NOT the global coords 
+            // so its moves relative to the centre of its parent.
+
+            //Randomly assigning scale
+            Vector3 scale = Vector3.zero;// so for scale we have min and max value for each diimension, x holds min values and y holds max
+            scale.x = Random.Range(sphereScaleRangeX.x, sphereScaleRangeX.y);
+            scale.y = Random.Range(sphereScaleRangeY.x, sphereScaleRangeY.y);
+            scale.z = Random.Range(sphereScaleRangeZ.x, sphereScaleRangeZ.y);
+
+            //adjust y scale by x distance from core
+            print("Offset X: " + offset.x);
+            print("Sphere Offset X: " + sphereOffsetScale.x);
+            scale.y *= 1 - (Mathf.Abs(offset.x) / sphereOffsetScale.x); //y scale is changed based on how far it is from the centre, the further out X is, the smaller the y scale  
+            scale.y = Mathf.Max(scale.y, scaleYMin);
+
+            sphereTrans.localScale = scale; //scale is always relative to the parents transform
+        }
+    }
+
+    private void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    Restart();
+        //}
+    }
+
+    private void Restart()
+    {
+        foreach (GameObject sphere in spheres)
+        {
+            Destroy(sphere);
+        }
+        Start();
+    }
+}
---
+++ Mission Demolition/Assets/Scripts/CloudCrafter.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudCrafter : MonoBehaviour
+{
+    [Header("SEt in Inspector")]
+    public int numClouds = 40; // the number of clouds to make
+    public GameObject cloudPrefab; // the prefab for the clouds
+    public Vector3 cloudPosMin = new Vector3(-50, -5, 10);
+    public Vector3 cloudPosMax = new Vector3(150, 100, 10);
+    public float cloudScaleMin = 1; //Min scale of each cloud
+    public float cloudScaleMax = 3; //MAX scale of each cloud
+    public float cloudSpeedMul = 0.5f; //adjusts speed of clouds
+    private GameObject[] cloudInstances;
+
+    private void Awake()
+    {
+        //make an array large enough to hold all the cloud instances
+        cloudInstances = new GameObject[numClouds];
+        //Find the CloudAnchor parent GameObject
+        GameObject anchor = GameObject.Find("CloudAnchor");
+        //Iterte through and make Cloud_s
+        GameObject cloud;
+
+        for (int i = 0; i < numClouds; i++)
+        {
+            //make an instance of cloudPrefab
+            cloud = Instantiate<GameObject>(cloudPrefab);
+            // position cloud
+            Vector3 cPos = Vector3.zero;
+            cPos.x = Random.Range(cloudPosMin.x, cloudPosMax.x);
+            cPos.y = Random.Range(cloudPosMin.y, cloudPosMax.y);
+            //scale cloud
+            float scaleU = Random.value;
+            float scaleValue = Mathf.Lerp(cloudScaleMin, cloudScaleMax, scaleU);
+            //scale clouds(with samller scaleU) should be earer the ground
+            cPos.y = Mathf.Lerp(cloudPosMin.y, cPos.y, scaleU);
+            //sma;;er clouds should be farther away
+            cPos.z = 100 - 90 * scaleU;
+            // apply these transform to the cloud
+            cloud.transform.position = cPos;
+            cloud.transform.localScale = Vector3.one * scaleValue; //ensures it is a vector
+            //make cloud a child of the anchor
+            cloud.transform.SetParent(anchor.transform);
+            //add the cloud to cloudInstances
+            cloudInstances[i] = cloud;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Iterate over each cloud that was created
+        foreach (var cloud in cloudInstances)
+        {
+            //get the cloud scale and postion
+            float scaleVal = cloud.transform.localScale.x;
+            Vector3 cloudPos = cloud.transform.position;
+            //Move larger clouds faster
+            cloudPos.x -= scaleVal * Time.deltaTime * cloudSpeedMul;
+
+            //if a cloud has move too far to the left..
+            if (cloudPos.x <= cloudPosMin.x)
+            {
+                //move it to the far right
+                cloudPos.x = cloudPosMax.x;
+            }
+            //apply the new position to the ckoud
+            cloud.transform.position = cloudPos;
+        }
+
+    }
+}
