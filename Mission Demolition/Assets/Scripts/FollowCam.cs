1564326e48f7debe193a546ae64a5ace814e8292
Isfand <izzywhizzzz@gmail.com>
2019-02-26 12:56:47 +0000

    Corrected a few issues realting to camera follow movement of the projectile

--- Mission Demolition/Assets/Scripts/FollowCam.cs
+++ Mission Demolition/Assets/Scripts/FollowCam.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class FollowCam : MonoBehaviour
 {
-    static public GameObject POI; // The static point of interest (ie. the projectile) can be access anywhere inc Slingshot scri[t
+    static public GameObject POI; // The static point of interest (ie. the projectile) can be access anywhere inc Slingshot script
+
+    [Header("Set in Inspector")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero; // [0,0]
 
     [Header("Set Dynamically")]
     public float camZ; // The desired Z pos of the camera
@@ -12,7 +16,6 @@ public class FollowCam : MonoBehaviour
     private void Awake()
     {
         camZ = this.transform.position.z; //intial camera position
-        print("Z: " + camZ);
     }
 
     //remembered called a fix number of times 50
@@ -23,6 +26,18 @@ private void FixedUpdate()
 
         //Get the position of the poi
         Vector3 destination = POI.transform.position;
+        // Limit the X & Y to minimum values
+        destination.x = Mathf.Max(minXY.x, destination.x); //note when the projectile is fired, it moves -ve in the X direction, this prevents the canmera from moving to left  of x = 0
+        destination.y = Mathf.Max(minXY.y, destination.y); //the same principle applies but to the y axis, so the projectiles doesn't go below y = 0
+
+        //Set the OrthographucSize of the camera to keep ground in view
+        Camera.main.orthographicSize = destination.y + 10; //we know from below that the minimum destination.y can't be below 0 so adding 10 (mimnimum otherogprahic size).
+        //This valye will nicely expland as the projectile moves upwards.
+
+        //Interpolate for the current Camera positon toward destination, Lerp basically tries to select a point between the first 2 parameters based on the 3rd value,
+        // I set it to 5%, doing this will tell Unity to move the camera about 5% of the way from its current location to the location of the POI every fixedUpdated
+        // bc POI is constanly moving, the transition should be smooth.
+        destination = Vector3.Lerp(transform.position, destination, easing); 
         //Force destination.z to be camZ to keep the camera far enough away. Otherwise it would be zoomed up
         destination.z = camZ;
         //set the camera to the destination
